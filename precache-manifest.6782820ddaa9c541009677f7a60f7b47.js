f2ae40df9ee4c363a9a3933e12dd86be6a62247e
Guilherme Modugno <g.modugno@hotmail.com>
2020-02-24 19:57:57 -0300

    Updates

---
+++ precache-manifest.6782820ddaa9c541009677f7a60f7b47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "585ab3dcca9b975802afd27dbd205099",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "013af652367249812f9b",
+    "url": "/monsters-rolodex/static/css/main.9c24544c.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "013af652367249812f9b",
+    "url": "/monsters-rolodex/static/js/main.5979edc2.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
